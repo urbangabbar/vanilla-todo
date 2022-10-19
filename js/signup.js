8a459e60dcad71bbc6b91e6fc871b798fae2c4e6
Abhinav Personal <abhinavpersonal@Abhinavs-MacBook-Pro.local>
2022-10-19 20:31:22 +0530

    Create a signup form for todo app

---
+++ js/signup.js
@@ -0,0 +1,35 @@
+// validate username to be unique and username and password should be more than 4 characters
+window.addEventListener("load", () => {
+  document
+    .querySelector("#signup-submit")
+    .addEventListener("click", handleSubmit);
+});
+
+function handleSubmit(e) {
+  // to avoid reloading
+  e.preventDefault();
+  const username = document.querySelector("#exampleInputEmail1").value;
+  const password = document.querySelector("#exampleInputPassword1").value;
+  if (validateCredentials(username, password)) {
+    createNewUser(username, password);
+  }else{
+    document.querySelector("#liveAlertPlaceholder").innerHTML = `<div class="alert alert-danger" role="alert">
+    Please check username password they should be greater than 4 characters and unique.
+  </div>`
+  }
+}
+
+function createNewUser(username, password) {
+  localStorage.setItem(username, password);
+  window.location.assign("./app.html");
+}
+
+function validateCredentials(username, password) {
+  if (!(username.length > 4 && password.length > 4)) {
+    return false;
+  }
+  if(localStorage.getItem(username)){
+    return false
+  }
+  return true;
+}
